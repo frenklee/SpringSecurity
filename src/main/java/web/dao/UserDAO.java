532346956b15e8a3c348b37da380689c61ac8f80
Li Maxim <FrenkLee365@gmail.com>
2021-06-04 20:40:26 +0900

    Commit 1

---
+++ src/main/java/web/dao/UserDAO.java
@@ -0,0 +1,13 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    public List<User> listUsers();
+    public User getUser(int id);
+    public void updateUser(int id, User user);
+    public void deleteUser(int id);
+    public void addUser(User user);
+}
